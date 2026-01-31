eb5a8e726e72a2cdec39edba7fdfb616e9d250a8
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-01-31 15:14:31 +0200

    feat: add About and Services components with respective HTML, SCSS, and TypeScript files
    - Created About component with layout and styling for legal services description.
    - Added Services component detailing legal protections and consultations.
    - Implemented responsive design for both components to enhance mobile usability.

---
+++ src/app/layout/Components/servies/servies.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servies',
+  standalone: false,
+  templateUrl: './servies.html',
+  styleUrl: './servies.scss',
+})
+export class Servies {
+
+}
--- src/app/layout/layout-module.ts
+++ src/app/layout/layout-module.ts
@@ -5,12 +5,16 @@ import { LayoutRoutingModule } from './layout-routing-module';
 import { MainPage } from './Components/main-page/main-page';
 import { HeroSection } from './Components/hero-section/hero-section';
 import { SharedModule } from '../shared/shared-module';
+import { About } from './Components/about/about';
+import { Servies } from './Components/servies/servies';
 
 
 @NgModule({
   declarations: [
     MainPage,
-    HeroSection
+    HeroSection,
+    About,
+    Servies
   ],
   imports: [
     CommonModule,
