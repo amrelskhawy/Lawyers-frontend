ab01df65651ceea5bd8840a7e7d262164a8c728a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-02-24 21:43:53 +0200

    feat: implement NoData component with HTML, SCSS, and TypeScript files

---
+++ src/app/shared/no-data/no-data.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-data',
+  standalone: false,
+  templateUrl: './no-data.html',
+  styleUrl: './no-data.scss',
+})
+export class NoData {
+
+}
