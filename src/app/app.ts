0485d2aadf25fc54e6a62b7f223d395b94e3f8ed
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-03-04 14:41:43 +0200

    add top scroll in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   standalone: false,
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('LawyeringFrelancer');
+
+  ngOnInit(): void {
+   this.scrollToTop();
+
+  }
+
+  scrollToTop() {
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
