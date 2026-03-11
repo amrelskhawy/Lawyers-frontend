902ce9f266694ceea226ec4c3ae1495c8e0c3e69
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-03-11 15:04:21 +0200

    teamwork component

---
+++ src/app/layout/Components/team-work/team-work.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-work',
+  standalone: false,
+  templateUrl: './team-work.html',
+  styleUrl: './team-work.scss',
+})
+export class TeamWork implements OnInit{
+ ngOnInit() {
+  }
+
+}
