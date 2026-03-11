12056e8c0570d0aa0924c36f9e008a00be2a4c3d
amrelskhawy <amrelskhawy@gmail.com>
2026-03-11 23:11:50 +0200

    refactor: Remove the `how-it-work` component, redesign `counter` styles, and update the `team-work` component with new internationalization keys.

--- src/app/layout/Components/team-work/team-work.ts
+++ src/app/layout/Components/team-work/team-work.ts
@@ -6,8 +6,97 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './team-work.html',
   styleUrl: './team-work.scss',
 })
-export class TeamWork implements OnInit{
- ngOnInit() {
-  }
+export class TeamWork implements OnInit {
+  teamMembers = [
+    {
+      name: 'د سعد البقمي',
+      image: '/assets/Img/المؤسس والرئيس التنفيذي دسعد البقمي.jpeg',
+      role: 'المؤسس والرئيس التنفيذي',
+    },
+    {
+      name: 'دكتور عبد الرحمن',
+      image: '/assets/Img/دكتور عبد الرحمن.jpeg',
+      role: 'مستشار قانوني',
+    },
+    {
+      name: 'سعد الحارثي',
+      image: '/assets/Img/سعد الحارثي.jpeg',
+      role: 'محامي',
+    },
+    {
+      name: 'حلا البلبيسي',
+      image: '/assets/Img/حلا البلبيسي.jpeg',
+      role: 'محامية',
+    },
+    {
+      name: 'عفاف الخشرمي',
+      image: '/assets/Img/عفاف الخشرمي.jpeg',
+      role: 'مستشارة قانونية',
+    },
+    {
+      name: 'نور عز الدين',
+      image: '/assets/Img/نور عز الدين.jpeg',
+      role: 'محامية',
+    },
+    {
+      name: 'انار',
+      image: '/assets/Img/انار.jpeg',
+      role: 'مساعد قانوني',
+    },
+    {
+      name: 'جشيم',
+      image: '/assets/Img/جشيم.jpeg',
+      role: 'مساعد إداري',
+    },
+    {
+      name: 'محمد الخربوش',
+      image: '/assets/Img/محمد الخربوش.jpeg',
+      role: 'محامي',
+    },
+    {
+      name: 'عمران الباشا',
+      image: '/assets/Img/عمران الباشا.jpeg',
+      role: 'مترجم قانوني',
+    },
+    {
+      name: 'محمد خليل',
+      image: '/assets/Img/mohamed-khalil.png',
+      role: 'مهندس برمجيات',
+    },
+  ];
 
+  responsiveOptions = [
+    {
+      breakpoint: '2000px',
+      numVisible: 6,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '1600px',
+      numVisible: 5,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '1300px',
+      numVisible: 4,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '992px',
+      numVisible: 3,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 1,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
+
+  ngOnInit() { }
 }
