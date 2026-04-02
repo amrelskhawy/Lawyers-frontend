6f5b4c3b96bad2b4ddb5284f725c98685950168a
AsmaaEbrhimes <zerowepschool@gmail.com>
2026-04-02 11:12:50 +0200

    conver img from jpg to avif and add file anmation in scss

--- src/app/layout/Components/team-work/team-work.ts
+++ src/app/layout/Components/team-work/team-work.ts
@@ -10,57 +10,57 @@ export class TeamWork implements OnInit {
   teamMembers = [
     {
       name: 'د سعد البقمي',
-      image: '/assets/Img/المؤسس والرئيس التنفيذي دسعد البقمي.jpeg',
+      image: '/assets/Img/المؤسس والرئيس التنفيذي دسعد البقمي.avif',
       role: 'المؤسس والرئيس التنفيذي',
     },
     {
       name: 'دكتور عبد الرحمن',
-      image: '/assets/Img/دكتور عبد الرحمن.jpeg',
+      image: '/assets/Img/دكتور عبد الرحمن.avif',
       role: 'مستشار قانوني',
     },
     {
       name: 'سعد الحارثي',
-      image: '/assets/Img/سعد الحارثي.jpeg',
+      image: '/assets/Img/سعد الحارثي.avif',
       role: 'محامي',
     },
     {
       name: 'حلا البلبيسي',
-      image: '/assets/Img/حلا البلبيسي.jpeg',
+      image: '/assets/Img/حلا البلبيسي.avif',
       role: 'محامية',
     },
     {
       name: 'عفاف الخشرمي',
-      image: '/assets/Img/عفاف الخشرمي.jpeg',
+      image: '/assets/Img/عفاف الخشرمي.avif',
       role: 'مستشارة قانونية',
     },
     {
       name: 'نور عز الدين',
-      image: '/assets/Img/نور عز الدين.jpeg',
+      image: '/assets/Img/نور عز الدين.avif',
       role: 'محامية',
     },
     {
       name: 'انار',
-      image: '/assets/Img/انار.jpeg',
+      image: '/assets/Img/انار.avif',
       role: 'مساعد قانوني',
     },
     {
       name: 'جشيم',
-      image: '/assets/Img/جشيم.jpeg',
+      image: '/assets/Img/جشيم.avif',
       role: 'مساعد إداري',
     },
     {
       name: 'محمد الخربوش',
-      image: '/assets/Img/محمد الخربوش.jpeg',
+      image: '/assets/Img/محمد الخربوش.avif',
       role: 'محامي',
     },
     {
       name: 'عمران الباشا',
-      image: '/assets/Img/عمران الباشا.jpeg',
+      image: '/assets/Img/عمران الباشا.avif',
       role: 'مترجم قانوني',
     },
     {
       name: 'محمد خليل',
-      image: '/assets/Img/mohamed-khalil.png',
+      image: '/assets/Img/mohamed-khalil.avif',
       role: 'مهندس برمجيات',
     },
   ];
