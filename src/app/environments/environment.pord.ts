0afc30488d1d4cfffb869fa09ed40ec37294ba3d
amrelskhawy <amrelskhawy@gmail.com>
2026-05-16 18:40:55 +0300

    fix: update Google Tag Manager ID and adjust environment configurations

--- src/app/environments/environment.pord.ts
+++ src/app/environments/environment.pord.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseApi: "https://www.saadalboqami.com/api/v1/",
+    baseApi: "http://localhost:8000/api/v1/",
     production: true,
-    gtmId: 'GTM-PWZKGMHH',
+    gtmId: 'G-0HMP8N28B2',
 }
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     baseApi: "http://localhost:8000/api/v1/",
     production: false,
-    gtmId: 'GTM-PWZKGMHH',
+    gtmId: 'G-0HMP8N28B2',
 }
