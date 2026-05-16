ce08b76c4758dd1b75b77f905d4028733d0f1683
amrelskhawy <amrelskhawy@gmail.com>
2026-05-16 18:47:00 +0300

    return base url for production

--- src/app/environments/environment.pord.ts
+++ src/app/environments/environment.pord.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    baseApi: "http://localhost:8000/api/v1/",
+    baseApi: "https://www.saadalboqami.com/api/v1/",
     production: true,
     gtmId: 'G-0HMP8N28B2',
 }
